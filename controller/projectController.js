0dc49a59d8d89b329d86865de670bbcfe4ffec6d
lukecode <lucassocorrosilva@gmail.com>
2022-11-21 15:15:11 -0300

    Criando novo controller para os projetos

---
+++ controller/projectController.js
@@ -0,0 +1,26 @@
+const obterProjects = async (req, res) => {};
+
+const newProject = async (req, res) => {};
+
+const obterProject = async (req, res) => {};
+
+const editProject = async (req, res) => {};
+
+const deleteProject = async (req, res) => {};
+
+const addCollaborator = async (req, res) => {};
+
+const deleteCollaborator = async (req, res) => {};
+
+const obterTasks = async (req, res) => {};
+
+export {
+  obterProjects,
+  newProject,
+  obterProject,
+  editProject,
+  deleteProject,
+  addCollaborator,
+  deleteCollaborator,
+  obterTasks,
+};

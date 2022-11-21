ba13ca8f57676eb18f34c988a1f235dd1407ba8f
lukecode <lucassocorrosilva@gmail.com>
2022-11-21 15:10:29 -0300

    Criando model de projetos

---
+++ model/Project.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const projectSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    deliveryDate: {
+      type: Date,
+      default: Date.now(),
+    },
+    client: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    create: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    collaborators: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Project = mongoose.model("Project", projectSchema);
+
+export default Project;

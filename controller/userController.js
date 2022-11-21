06fad1fe7cf383e97b6cd3c27e70aac6f359d52e
lukecode <lucassocorrosilva@gmail.com>
2022-11-21 09:05:13 -0300

    Finalizando config de usuários

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,19 @@
 import User from "../model/User.js";
 
 const register = async (req, res) => {
+  const { email } = req.body;
+  const existUser = await User.findOne({ email: email });
+
+  if (existUser) {
+    const error = new Error("Usuário registrado");
+    return res.status(400).json({ msg: error.message });
+  }
+
   try {
     const user = new User(req.body);
     const userDB = await user.save();
-    res.json(userDB)
-    console.log(user)
+    res.json(userDB);
+    console.log(user);
   } catch (error) {
     console.log(error.message);
   }

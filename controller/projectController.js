88c2005d449db582be1e933d4828ea25a9747b48
lukecode <lucassocorrosilva@gmail.com>
2022-11-25 12:16:22 -0300

    Finalizando controller de projetos

--- controller/projectController.js
+++ controller/projectController.js
@@ -20,24 +20,80 @@ const newProject = async (req, res) => {
 const obterProject = async (req, res) => {
   const { id } = req.params;
 
-  const project = await Project.findById(id);
+  if (id.match(/^[0-9a-fA-F]{24}$/)) {
+    const project = await Project.findById(id.trim());
 
-  if (!project) {
-    const error = new Error("Não encontrado");
+    if (!project) {
+      const error = new Error("Projeto não existe");
+      return res.status(404).json({ msg: error.message });
+    }
+
+    if (project.created.toString() !== req.user._id.toString()) {
+      const error = new Error("Ação invalida");
+      return res.status(401).json({ msg: error.message });
+    }
+
+    res.json(project);
+  } else {
+    const error = new Error("Id inválido");
     return res.status(404).json({ msg: error.message });
   }
-
-  if (project.created.toString() !== req.user._id.toString()) {
-    const error = new Error("Ação invalida");
-    return res.status(404).json({ msg: error.message });
-  }
-
-  res.json(project);
 };
 
-const editProject = async (req, res) => {};
+const editProject = async (req, res) => {
+  const { id } = req.params;
+  if (id.match(/^[0-9a-fA-F]{24}$/)) {
+    const project = await Project.findById(id.trim());
 
-const deleteProject = async (req, res) => {};
+    if (!project) {
+      const error = new Error("Projeto não existe");
+      return res.status(404).json({ msg: error.message });
+    }
+
+    if (project.created.toString() !== req.user._id.toString()) {
+      const error = new Error("Ação invalida");
+      return res.status(401).json({ msg: error.message });
+    }
+    project.name = req.body.name || project.name;
+    project.description = req.body.description || project.description;
+    project.deliveryDate = req.body.deliveryDate || project.deliveryDate;
+    project.client = req.body.client || project.client;
+
+    try {
+      const projectSave = await project.save();
+      res.json(projectSave);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+const deleteProject = async (req, res) => {
+  const { id } = req.params;
+
+  if (id.match(/^[0-9a-fA-F]{24}$/)) {
+    const project = await Project.findById(id.trim());
+
+    if (!project) {
+      const error = new Error("Projeto não existe");
+      return res.status(404).json({ msg: error.message });
+    }
+
+    if (project.created.toString() !== req.user._id.toString()) {
+      const error = new Error("Ação invalida");
+      return res.status(401).json({ msg: error.message });
+    }
+    try {
+      await project.deleteOne();
+      res.json({ msg: "Projeto Eliminado" });
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    const error = new Error("Id inválido");
+    return res.status(404).json({ msg: error.message });
+  }
+};
 
 const addCollaborator = async (req, res) => {};
 

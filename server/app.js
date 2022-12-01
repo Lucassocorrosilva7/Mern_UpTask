8e61a3b354dc2eecb047aabf468a67a52f451307
lukecode <lucassocorrosilva@gmail.com>
2022-12-01 15:36:49 -0300

    Habilitando cors

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDb from './config/db.js';
-import userRoutes from './routes/userRoutes.js';
-import projectRouter from './routes/projectRouter.js';
-import taskRoutes from './routes/taskRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDb from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import projectRouter from "./routes/projectRouter.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -13,11 +13,23 @@ dotenv.config();
 
 connectDb();
 
-app.use('/api/users', userRoutes);
-app.use('/api/projects', projectRouter);
-app.use('/api/tasks', taskRoutes);
+const whiteList = ["http://127.0.0.1:5173"];
 
-app.use(cors());
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de Cors"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+app.use("/api/users", userRoutes);
+app.use("/api/projects", projectRouter);
+app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 4000;
 

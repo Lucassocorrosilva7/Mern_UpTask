301606902fc11e3899ba6198ad86ef2fff6451d7
lukecode <lucassocorrosilva@gmail.com>
2022-12-01 11:24:27 -0300

    Página de login criada

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,62 @@
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize text-center">
+        Íniciar seção
+      </h1>
+      <form className="my-10 bg-white shadow rounded  p-10">
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email de Registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="password"
+          >
+            Senha
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Senha de Registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Íniciar Sessão"
+          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="registar"
+        >
+          Não tem uma conta? Registre-se
+        </Link>
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="recuperar-senha"
+        >
+          Esqueci minha senha
+        </Link>
+      </nav>
+    </>
+  );
+};
 
-export default Login
+export default Login;

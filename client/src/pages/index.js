c0448c3c1688cab1d3b6b2df462d3481e0867867
lukecode <lucassocorrosilva@gmail.com>
2022-12-12 14:21:40 -0300

    Organizando importações de pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import Login from "./Login";
+import Register from "./Register";
+import ConfirmAccount from "./ConfirmAccount";
+import NewPassword from "./NewPassword";
+import RecoverPassword from "./RecoverPassword";
+
+export { Login, Register, ConfirmAccount, NewPassword, RecoverPassword };

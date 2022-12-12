b1930b47290702cc87c7c093e92c1952a3cb9cf6
lukecode <lucassocorrosilva@gmail.com>
2022-12-12 14:20:25 -0300

    Add AuthProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthLayout from "./layouts/AuthLayout";
-import { Login, Register, ConfirmAccount, NewPassword, RecoverPassword } from "./pages";
+import {
+  Login,
+  Register,
+  ConfirmAccount,
+  NewPassword,
+  RecoverPassword,
+} from "./pages";
+import { AuthProvider } from "./context/AuthProvider";
 
-  function App(){
-    return (
-      <BrowserRouter>
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
         <Routes>
-            <Route path="/" element={<AuthLayout/>}>
-                <Route index element={<Login/>} />
-                <Route path="registrar" element={<Register/>} />
-                <Route path="recuperar-senha" element={<RecoverPassword/>} />
-                <Route path="recuperar-senha/:token" element={<NewPassword/>} />
-                <Route path="confirmar/:id" element={<ConfirmAccount/>} />
-            </Route>
+          <Route path="/" element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path="registrar" element={<Register />} />
+            <Route path="recuperar-senha" element={<RecoverPassword />} />
+            <Route path="recuperar-senha/:token" element={<NewPassword />} />
+            <Route path="confirmar/:id" element={<ConfirmAccount />} />
+          </Route>
         </Routes>
-      </BrowserRouter>
-    )
-  }
-
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
 
 export default App;

4865d34854fdd7268c93b12872147fc3bfbaffd3
lukecode <lucassocorrosilva@gmail.com>
2022-12-13 11:22:22 -0300

    Criando rota e página de novo projeto

---
+++ client/src/pages/NewProject.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewProject = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-black">Novo Projeto</h1>
+      <div></div>
+    </>
+  );
+};
+
+export default NewProject;

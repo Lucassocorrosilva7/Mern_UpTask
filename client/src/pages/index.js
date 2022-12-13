aa9b567913972a3f364d6dc5e94509522a2e2ced
lukecode <lucassocorrosilva@gmail.com>
2022-12-13 11:24:54 -0300

    Importando páginas

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,5 +3,15 @@ import Register from "./Register";
 import ConfirmAccount from "./ConfirmAccount";
 import NewPassword from "./NewPassword";
 import RecoverPassword from "./RecoverPassword";
+import Projects from "./Projects";
+import NewProject from "./NewProject";
 
-export { Login, Register, ConfirmAccount, NewPassword, RecoverPassword };
+export {
+  Login,
+  Register,
+  ConfirmAccount,
+  NewPassword,
+  RecoverPassword,
+  Projects,
+  NewProject
+};

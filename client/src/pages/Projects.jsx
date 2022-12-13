b9f8746249ad9b565dd3b9047fd9847b9b016d86
lukecode <lucassocorrosilva@gmail.com>
2022-12-13 11:21:51 -0300

    Criando rota e  página de projetos

---
+++ client/src/pages/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+   <>
+    <h1 className='text-4xl font-black'>Projetos</h1>
+   <div>
+    
+   </div>
+   </>
+  )
+}
+
+export default Projects

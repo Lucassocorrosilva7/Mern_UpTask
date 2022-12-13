7895bfe7f75d5c3bfd9682e199528f09be3b85c0
lukecode <lucassocorrosilva@gmail.com>
2022-12-13 11:25:52 -0300

    Add mais config no authprovider

--- client/src/context/AuthProvider.jsx
+++ client/src/context/AuthProvider.jsx
@@ -1,22 +1,56 @@
 import { useState, useEffect, createContext } from "react";
+import { useNavigate } from "react-router-dom";
+import clientAxios from "../config/clientAxios";
 
 const AuthContext = createContext();
 
-const AuthProvider = ({children}) => {
-  return (
-    <AuthContext.Provider 
-        value={{
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({});
+  const [loading, SetLoading] = useState(true);
+  const navigate = useNavigate();
 
-        }}
+
+  useEffect(() => {
+    const authenticationUser = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        SetLoading(false);
+        return;
+      }
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      try {
+        const { data } = await clientAxios("/users/perfil", config);
+        setAuth(data);
+        navigate('/projetos');
+      } catch (error) {
+        setAuth({});
+      }
+
+      SetLoading(false);
+    };
+    authenticationUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        auth,
+        setAuth,
+        loading,
+      }}
     >
-        {children}
+      {children}
     </AuthContext.Provider>
   );
 };
 
-export {
-    AuthProvider
-}
-
+export { AuthProvider };
 
 export default AuthContext;

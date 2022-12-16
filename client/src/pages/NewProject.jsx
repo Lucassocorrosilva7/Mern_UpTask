60e35c78594d92fc14ac0930b680ef154f089e48
lukecode <lucassocorrosilva@gmail.com>
2022-12-16 11:13:42 -0300

    Add componente de form para criação do projeto

--- client/src/pages/NewProject.jsx
+++ client/src/pages/NewProject.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import FormProject from "../components/FormProject";
 
 const NewProject = () => {
   return (
     <>
-      <h1 className="text-4xl font-black">Novo Projeto</h1>
-      <div></div>
+      <h1 className="text-4xl font-black">Criar Projeto</h1>
+      <div className="mt-10 flex justify-center">
+          <FormProject/>
+      </div>
     </>
   );
 };

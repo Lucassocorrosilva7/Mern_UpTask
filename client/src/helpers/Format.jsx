6253ebfa0fc00cb168f36d5c4b824a82f1ef7af0
lukecode <lucassocorrosilva@gmail.com>
2022-12-27 18:20:08 -0300

    Criando formatação de data

--- client/src/helpers/Format.jsx
+++ client/src/helpers/Format.jsx
@@ -1,10 +1,12 @@
-export const format = date => {
-    const newDate = new Date(date)
+export const format = (date) => {
+  const newDate = new Date(date);
 
-    const options = {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-    }
-} 
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return newDate.toLocaleDateString("pt-BR", options);
+};

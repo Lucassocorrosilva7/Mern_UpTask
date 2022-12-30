3956b0834019c22e1ec407634a86150f734dbd70
lukecode <lucassocorrosilva@gmail.com>
2022-12-29 21:40:30 -0300

    Add estilo e passando lógica  para add colaborador ao projeto

--- client/src/pages/NewContributor.jsx
+++ client/src/pages/NewContributor.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { useEffect } from "react";
+import FormContributor from "../components/FormContributor";
+import useProjects from "../hooks/useProjects";
+import { useParams } from "react-router-dom";
+import Alert from "../components/Alert";
 
 const NewContributor = () => {
-  return (
-    <div>NewContributor</div>
-  )
-}
+  const { obterProject, project, loading, contributor, addContributor, alert } = useProjects();
 
-export default NewContributor
+  const params = useParams();
+
+  useEffect(() => {
+    obterProject(params.id);
+  }, []);
+
+  if (loading) return "Loading...";
+
+  const { name } = project;
+
+  if(!project?._id) return <Alert alert={alert}/>
+  return (
+    <>
+      <h1 className="text-3xl font-black">
+        Adicionar Colaborador(a), Projeto: {name}
+      </h1>
+
+      <div className="mt-10 flex justify-center">
+        <FormContributor />
+      </div>
+
+      {loading ? (
+        <p className="text-center">Carregando</p>
+      ) : (
+        contributor?._id && (
+          <div className="flex justify-center mt-10">
+            <div className="bg-white py-10 px-5 md:w-1/2 rounded-lg shadow">
+              <h2 className="text-center mb-10 text-2xl font-bold">
+                Resultado:{" "}
+              </h2>
+              <div className="flex justify-between items-center">
+                <p>{contributor.name}</p>
+                <button
+                  type="button"
+                  className="bg-sky-500 hover:bg-sky-600 transition-colors px-5 py-2 rounded-lg uppercase text-white font-bold text-sm"
+                  onClick={() => addContributor({
+                    email: contributor.email
+                  })}
+                >
+                  Adicionar Projeto
+                </button>
+              </div>
+            </div>
+          </div>
+        )
+      )}
+    </>
+  );
+};
+
+export default NewContributor;

95e9a9f7efceb2718324d15803f476b9bfde2a93
lukecode <lucassocorrosilva@gmail.com>
2023-01-01 20:19:46 -0300

    Sincronizando projeto comcolaborador

--- client/src/components/PreviewProject.jsx
+++ client/src/components/PreviewProject.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const PreviewProject = ({ project }) => {
-  const { name, _id, client } = project;
+  const { auth } = useAuth();
+
+  const { name, _id, client, created } = project;
+
   return (
-    <div className="border-b p-5 flex items-center">
-      <p className="flex-1">{name} - 
-        <span className="text-sky-500 capitalize">{''} {client}</span>
+    <div className="border-b p-5 flex items-center justify-between">
+     <div className="flex items-center gap-2">
+       <p className="flex-1">
+        {name} -
+        <span className="text-sky-500 capitalize">
+          {""} {client}
+        </span>
       </p>
+      {auth._id !== created && <p className="p-1.5 text-xs rounded text-gray-900 bg-green-400 font-bold uppercase">Colaborador</p>}
+     </div>
       <Link
         className="bg-sky-600 hover:bg-sky-700 transition-colors p-3 rounded-sm text-white  uppercase text-sm font-bold"
         to={`${_id}`}

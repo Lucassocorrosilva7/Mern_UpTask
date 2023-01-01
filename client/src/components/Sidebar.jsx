7c8c6c41c11e06298a2aecbaf6cfdcac7e4c767e
lukecode <lucassocorrosilva@gmail.com>
2023-01-01 20:41:58 -0300

    Passando breakpoints  para  sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -6,8 +6,8 @@ const Sidebar = () => {
   const { auth } = useAuth();
   const { name } = auth;
   return (
-    <aside className="md:w-80 lg-96 px-5 py-10 bg-sky-200">
-      <p className="text-xl font-bold">Olá: {name}</p>
+    <aside className="md:w-1/3 lg:w-1/5 xl:w-1/6 px-5 py-10 bg-sky-200">
+      <p className="md:text-base lg:text-xl font-bold">Olá: {name}</p>
       <Link
         to="criar-projeto"
         className="bg-sky-600 w-full p-3 text-white uppercase font-bold block mt-5 text-center rounded-lg"
